39201ebefa332db337ca76ee9a3626b8a96a8ce4
Rinat <rynaldo@mail.ru>
2022-09-15 00:07:12 +0500

    Файл создания БД создает таблицы и заполняет начальными данными / modified a DB creation file, and filled in with initial data

--- create_testdb.php
+++ create_testdb.php
@@ -2,54 +2,60 @@
 	
 	require("db_login.php");
 
+	// Блок создаёт вновь тестовую БД и таблицы
 	try {
 
         $dbh = new PDO("mysql:host=$db_host", $db_user, $db_pass);
 
-        $dbh->exec("CREATE DATABASE IF NOT EXISTS `$db_name`;
+        $dbh->exec("DROP DATABASE IF EXISTS `$db_name`;
+        			CREATE DATABASE `$db_name`;
                 	CREATE USER `$db_user`@`$db_host` IDENTIFIED BY '$db_pass';
                 	GRANT ALL ON `$db_name`.* TO '$db_user'@'$db_host';
-                	FLUSH PRIVILEGES;")
-        or die(print_r($dbh->errorInfo(), true));
+                	FLUSH PRIVILEGES;");
+        //or die(print_r($dbh->errorInfo(), true));
 
         $dbh->exec("USE $db_name;
-                	CREATE TABLE game(game_id INT NOT NULL AUTO_INCREMENT,
-					  				  game VARCHAR(150),
-					  				  PRIMARY KEY(game_id));
-					CREATE TABLE studio(studio_id INT NOT NULL AUTO_INCREMENT,
+        			CREATE TABLE studio(studio_id INT NOT NULL AUTO_INCREMENT,
 										studio VARCHAR(150),
 										PRIMARY KEY(studio_id));
 					CREATE TABLE genre(genre_id INT NOT NULL AUTO_INCREMENT,
 									   genre VARCHAR(150),
 									   PRIMARY KEY(genre_id));
-					CREATE TABLE game_to_genres(game_d INT, genre_id INT);
-					CREATE TABLE game_to_studio(game_d INT, studio_id INT);")
-        or die(print_r($dbh->errorInfo(), true));
+                	CREATE TABLE game(game_id INT NOT NULL AUTO_INCREMENT,
+					  				  game VARCHAR(150),
+					  				  studio_id INT REFERENCES studio(studio_id),
+					  				  genre_id INT REFERENCES genre(genre_id),
+					  				  PRIMARY KEY(game_id));");
+		//or die(print_r($dbh->errorInfo(), true));
 
     }
     catch (PDOException $e) {
         die("DB ERROR: " . $e->getMessage());
     }
 
+    // Блок заполняет таблицы начальными данными
     try {
     	$games = ["Pac-Man", "Lode Runner", "Mario", "Syberia", "Counter-Strike"];
         $studios = ["Namco", "Broderbund&Ariolasoft", "Nintendo", "Microids", "Valve Corporation"];
         $genres = ["arcade", "platformer", "quest", "FPS", "RTS"];
+        $genres_id = [1, 2, 2, 3, 5];
+        $studios_id = [1, 3, 2, 4, 5];
 
         $dbh->exec("USE $db_name;");
 
-        foreach ($games as $game) {
-        	$dbh->exec("INSERT INTO game(game) VALUES('$game');");
-        }
-
         foreach ($studios as $studio) {
+        	echo "$studio <br>";
         	$dbh->exec("INSERT INTO studio(studio) VALUES('$studio');");
         }
 
         foreach ($genres as $genre) {
+        	echo "$genre <br>";
         	$dbh->exec("INSERT INTO genre(genre) VALUES('$genre');");
         }
 
+        for ($i=0; $i < sizeof($games); $i++) { 
+        	$dbh->exec("INSERT INTO game(game, studio_id, genre_id) VALUES('$games[$i]', $studios_id[$i], $genres_id[$i]);");
+        }
 
     } catch (PDOException $e) {
     	die("DB ERROR: " . $e->getMessage());
