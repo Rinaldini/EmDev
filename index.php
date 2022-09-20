d81e4bbf5e2752aaa8075547a701299e1f8a094a
Rinat <rynaldo@mail.ru>
2022-09-20 22:36:51 +0500

    Подключены файлы MVC / MVC files connected

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 	
 	require("create_testdb.php");
+
+	require("db.php");
+	require("view/View.php");
+	require("model/Model.php");
+	require("controller/Controller.php");
+
+	$control = new Controller();
+	$control->index();
+	
 	phpinfo();
 
 ?>
