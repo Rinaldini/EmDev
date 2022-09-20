b97a71bd8e8e5f51841830d6a1b6a56c843449b5
Rinat <rynaldo@mail.ru>
2022-09-20 21:51:37 +0500

    Создан начальный шаблон / Index template created

--- view/view.tpl.php
+++ view/view.tpl.php
@@ -6,8 +6,23 @@
 	<title>Document</title>
 </head>
 <body>
-	<?=
-		$data;
-	?>
+	<table>
+		<tr>
+			<th>Game</th>
+			<th>Studio</th>
+			<th>Genre</th>
+		</tr>
+		<tr>
+			<?php
+				foreach ($data as $row) {
+					echo '<tr class="row">';
+					echo '<td>' . $row['game'] . '</td>';
+					echo '<td>' . $row['studio'] . '</td>';
+					echo '<td>' . $row['genre'] . '</td>';
+					echo '</tr>';
+				}
+			?>
+		</tr>
+	</table>
 </body>
 </html>
